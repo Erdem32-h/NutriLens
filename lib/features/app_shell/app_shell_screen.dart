b57f1bb8249d8949bfb99d0de344c749aef0bd48
fatih <halkasosyal@gmail.com>
2026-03-21 18:23:23 +0300

    feat: replace BottomNavigationBar with Material3 NavigationBar

--- lib/features/app_shell/app_shell_screen.dart
+++ lib/features/app_shell/app_shell_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
 import '../../core/extensions/l10n_extension.dart';
+import '../../core/theme/app_colors.dart';
 
 class AppShellScreen extends StatelessWidget {
   final Widget child;
@@ -10,7 +11,7 @@ class AppShellScreen extends StatelessWidget {
 
   int _currentIndex(BuildContext context) {
     final location = GoRouterState.of(context).matchedLocation;
-    final paths = ['/scanner', '/history', '/favorites', '/profile'];
+    const paths = ['/scanner', '/history', '/favorites', '/profile'];
     for (var i = 0; i < paths.length; i++) {
       if (location.startsWith(paths[i])) return i;
     }
@@ -21,37 +22,46 @@ class AppShellScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final currentIndex = _currentIndex(context);
     final l10n = context.l10n;
-    final paths = ['/scanner', '/history', '/favorites', '/profile'];
-
-    final tabs = [
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.qr_code_scanner),
-        label: l10n.scanner,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.history),
-        label: l10n.history,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.favorite_outline),
-        label: l10n.favorites,
-      ),
-      BottomNavigationBarItem(
-        icon: const Icon(Icons.person_outline),
-        label: l10n.profile,
-      ),
-    ];
+    const paths = ['/scanner', '/history', '/favorites', '/profile'];
 
     return Scaffold(
+      backgroundColor: AppColors.background,
       body: child,
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: currentIndex,
-        onTap: (index) {
-          if (index != currentIndex) {
-            context.go(paths[index]);
-          }
-        },
-        items: tabs,
+      bottomNavigationBar: Container(
+        decoration: const BoxDecoration(
+          color: AppColors.surface,
+          border: Border(
+            top: BorderSide(color: AppColors.border, width: 0.5),
+          ),
+        ),
+        child: NavigationBar(
+          selectedIndex: currentIndex,
+          onDestinationSelected: (index) {
+            if (index != currentIndex) context.go(paths[index]);
+          },
+          destinations: [
+            NavigationDestination(
+              icon: const Icon(Icons.qr_code_scanner_outlined),
+              selectedIcon: const Icon(Icons.qr_code_scanner),
+              label: l10n.scanner,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.history_outlined),
+              selectedIcon: const Icon(Icons.history),
+              label: l10n.history,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.favorite_outline),
+              selectedIcon: const Icon(Icons.favorite),
+              label: l10n.favorites,
+            ),
+            NavigationDestination(
+              icon: const Icon(Icons.person_outline),
+              selectedIcon: const Icon(Icons.person),
+              label: l10n.profile,
+            ),
+          ],
+        ),
       ),
     );
   }
