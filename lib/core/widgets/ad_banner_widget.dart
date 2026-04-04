87e0d82591934eb5e6f9cf748ce9b109639eff0e
fatih <halkasosyal@gmail.com>
2026-04-04 21:35:17 +0300

    feat: add conditional AdMob banner widget

---
+++ lib/core/widgets/ad_banner_widget.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+import '../constants/ad_constants.dart';
+import '../providers/monetization_provider.dart';
+
+class AdBannerWidget extends ConsumerStatefulWidget {
+  const AdBannerWidget({super.key});
+
+  @override
+  ConsumerState<AdBannerWidget> createState() => _AdBannerWidgetState();
+}
+
+class _AdBannerWidgetState extends ConsumerState<AdBannerWidget> {
+  BannerAd? _bannerAd;
+  bool _isLoaded = false;
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (_bannerAd == null) _loadAd();
+  }
+
+  void _loadAd() {
+    final adSize = AdSize.getAnchoredAdaptiveBannerAdSize(
+      Orientation.portrait,
+      MediaQuery.of(context).size.width.truncate(),
+    );
+
+    if (adSize == null) return;
+
+    _bannerAd = BannerAd(
+      adUnitId: AdConstants.bannerAdUnitId,
+      size: adSize,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (_) {
+          if (mounted) setState(() => _isLoaded = true);
+        },
+        onAdFailedToLoad: (ad, error) {
+          ad.dispose();
+          debugPrint('[AdBanner] Failed to load: ${error.message}');
+        },
+      ),
+    )..load();
+  }
+
+  @override
+  void dispose() {
+    _bannerAd?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final isPremium = ref.watch(isPremiumProvider);
+
+    if (isPremium || !_isLoaded || _bannerAd == null) {
+      return const SizedBox.shrink();
+    }
+
+    return SizedBox(
+      width: _bannerAd!.size.width.toDouble(),
+      height: _bannerAd!.size.height.toDouble(),
+      child: AdWidget(ad: _bannerAd!),
+    );
+  }
+}
